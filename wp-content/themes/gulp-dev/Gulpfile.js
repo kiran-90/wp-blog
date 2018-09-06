5bbe5a9e3bcac185dc2405f97aeffea5a8bb44c4
Kiran <kiranchahal1@live.co.uk>
2018-09-06 17:05:12 +0100

    amended virtual url from .dev to .site due to Chrome update

--- wp-content/themes/gulp-dev/Gulpfile.js
+++ wp-content/themes/gulp-dev/Gulpfile.js
@@ -58,7 +58,7 @@ gulp.task('javascript', function() {
 gulp.task('watch', function() {
 	browserSync.init({ 
 		open: 'external',
-		proxy: 'kays.dev',
+		proxy: 'kays.site',
 		port: 8080
 	});
 	gulp.watch([root + '**/*.css', root + '**/*.scss' ], ['css']);
