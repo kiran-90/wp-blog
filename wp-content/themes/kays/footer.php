f2e36264f8512837a8e5d20957b9ffa3df7a15de
Kiran <kiranchahal1@live.co.uk>
2018-09-18 16:03:25 +0100

    apply styles to menu list

--- wp-content/themes/kays/footer.php
+++ wp-content/themes/kays/footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'kays' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'kays' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'kays' ), 'kays', '<a href="https://mor10.com/courses" rel="designer">Morten Rand-Hendriksen</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'kays' ), 'kays', '<a href="https://mor10.com/courses" rel="designer">Kirandeep Chahal</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
