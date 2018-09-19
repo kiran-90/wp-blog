a1e885ae24a8b046635c8989cfc608333e0d6382
Kiran <kiranchahal1@live.co.uk>
2018-09-19 15:04:31 +0100

    enable javascript dropdown menus

--- wp-content/themes/kays/functions.php
+++ wp-content/themes/kays/functions.php
@@ -173,9 +173,14 @@ function kays_scripts() {
         
 	wp_enqueue_style( 'kays-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'kays-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'kays-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20151215', true );
 
-	wp_enqueue_script( 'kays-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_localize_script( 'kays-navigation', 'kaysScreenReaderText', array(
+            'expand' => __( 'Expand child menu', 'kays'),
+            'collapse' => __( 'Collapse child menu', 'kays'),
+	));
+        
+        wp_enqueue_script( 'kays-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
