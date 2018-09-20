374511bec95b47aad107c186d83acc86819567ad
Kiran <kiranchahal1@live.co.uk>
2018-09-20 16:32:49 +0100

    modify output of post metadata

--- wp-content/themes/kays/inc/template-tags.php
+++ wp-content/themes/kays/inc/template-tags.php
@@ -25,17 +25,33 @@ function kays_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'kays' ),
+		esc_html_x( 'on %s', 'post date', 'kays' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
 	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'kays' ),
+		esc_html_x( 'Written by %s', 'post author', 'kays' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="byline"> ' . $byline . '</span> <span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
+        if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+            echo ' and has <span class="comments-link">';
+            /* translators: %s: post title */
+            comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'kays' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
+            echo '</span>';
+	}
+        
+        edit_post_link(
+            sprintf(
+                /* translators: %s: Name of current post */
+                esc_html__( 'Edit %s', 'kays' ),
+                the_title( '<span class="screen-reader-text">"', '"</span>', false )
+            ),
+		' <span class="edit-link">',
+		'</span>'
+	);
 }
 endif;
 
@@ -53,23 +69,6 @@ function kays_entry_footer() {
 			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'kays' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 		}
 	}
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		/* translators: %s: post title */
-		comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'kays' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
-		echo '</span>';
-	}
-
-	edit_post_link(
-		sprintf(
-			/* translators: %s: Name of current post */
-			esc_html__( 'Edit %s', 'kays' ),
-			the_title( '<span class="screen-reader-text">"', '"</span>', false )
-		),
-		'<span class="edit-link">',
-		'</span>'
-	);
 }
 endif;
 
