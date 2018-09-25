bf7f873c09b6ec97d604c3565d74f9febe77b209
Kiran <kiranchahal1@live.co.uk>
2018-09-25 13:38:57 +0100

    edit single post template for no sidebar layout

--- wp-content/themes/kays/template-parts/content-single.php
+++ wp-content/themes/kays/template-parts/content-single.php
@@ -19,7 +19,7 @@
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
-		if ( 'post' === get_post_type() ) : ?>
+		if ( is_active_sidebar('sidebar-1') ) : ?>
 		<div class="entry-meta">
 			<?php kays_posted_on(); ?>
 		</div><!-- .entry-meta -->
@@ -28,6 +28,18 @@
 	</header><!-- .entry-header -->
 
         <section class="post-content">
+            
+            <?php
+            
+            if ( !is_active_sidebar('sidebar-1') ) : ?>
+            <div class="post_content__wrap">
+            <div class="entry-meta">
+		<?php kays_posted_on(); ?>
+            </div><!-- .entry-meta -->
+            <div class="post_content__body">
+            <?php
+            endif; ?>
+            
             <div class="entry-content">
 		<?php
                     the_content( sprintf(
@@ -47,6 +59,12 @@
 		<?php kays_entry_footer(); ?>
             </footer><!-- .entry-footer -->
         
+            <?php          
+            if ( !is_active_sidebar('sidebar-1') ) : ?>
+            </div><!-- .post_content__body -->
+            </div><!-- .post_content__wrap -->
+            <?php endif; ?>
+            
             <?php
             kays_post_navigation();
 
