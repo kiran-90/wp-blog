c1b47938d66076c30fad18360358513935abdf10
Kiran <kiranchahal1@live.co.uk>
2018-09-30 17:51:18 +0100

    add custom SVG icon in theme

--- wp-content/themes/kays/index.php
+++ wp-content/themes/kays/index.php
@@ -42,8 +42,8 @@ get_header(); ?>
 			endwhile;
 
 			the_posts_pagination( array(
-				'prev_text' => kays_get_svg( array( 'icon' => 'arrow-left' ) ) . __( '  Newer', 'kays' ),
-				'next_text' => __( 'Older  ', 'kays' ) . kays_get_svg( array( 'icon' => 'arrow-right' ) ),
+				'prev_text' => kays_get_svg( array( 'icon' => 'arrow-point-left' ) ) . __( '  Newer', 'kays' ),
+				'next_text' => __( 'Older  ', 'kays' ) . kays_get_svg( array( 'icon' => 'arrow-point-right' ) ),
 				'before_page_number' => '<span class="screen-reader-text">' . __( 'Page ', 'kays' ) . '</span>',
 			));
 
